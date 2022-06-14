ec29702c82a52abbac2222cb4ffe4d2f0f73fa0f
Naren1305 <107239280+Naren1305@users.noreply.github.com>
2022-06-14 16:40:56 +0530

    Update bossbaby.java

--- bossbaby.java
+++ bossbaby.java
@@ -1,6 +1,5 @@
-/**  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
- */
-public class bossbaby {
+
+public class bossbaby{
   public static void main(String[] args) {
     System.out.println("Hello, World");
   }
